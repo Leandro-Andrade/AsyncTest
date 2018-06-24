8a3f13fb3fca68a1d9cdfdae18da659c06de2c6c
Leandro Andrade <leandroandrade@outlook.com.br>
2018-06-24 19:06:58 -0300

    Code Cleanup

---
+++ src/Evision/AccountService.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Evision
+{
+    public class AccountService : IAccountService
+    {
+        public async Task<double> GetAccountAmountAsync(int accountId)
+        {
+            await Task.Delay(5000);
+            return 0;
+        }
+    }
+}
--- src/Evision/IAccountService.cs
+++ src/Evision/IAccountService.cs
@@ -6,13 +6,4 @@ namespace Evision
     {
         Task<double> GetAccountAmountAsync(int accountId);
     }
-
-    public class AccountService : IAccountService
-    {
-        public async Task<double> GetAccountAmountAsync(int accountId)
-        {
-            await Task.Delay(5000);
-            return 0;
-        }
-    }
 }
